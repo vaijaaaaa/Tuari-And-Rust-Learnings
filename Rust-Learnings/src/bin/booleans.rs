c252c0567347108814ae7c3d3917f71c916ed2ec
Vaijnath Patil <vaijuwalker111@gmail.com>
2026-04-23 10:19:58 +0530

    Add examples for boolean values and arithmetic operations in Rust

---
+++ Rust-Learnings/src/bin/booleans.rs
@@ -0,0 +1,5 @@
+const is_programming_fun:bool = true;
+const is_fish_tasty:bool = false;
+
+println!("Is Programming Fun? {}", is_programming_fun);
+println!("Is Fish Tasty? {}",is_fish_tasty);
---
+++ Rust-Learnings/src/bin/operators.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let add = 5 +3;
+    let sub = 10-4;
+    let mul = 6 * 2;
+    let div = 12/3;
+    let rem = 10 %3;
+
+    println!("Add: {}",add);
+    println!("Sub :{}",sub);
+    println!("Mul:{}",mul);
+    println!("Div:{}",div);
+    println!("Rem:{}",rem);
+}
